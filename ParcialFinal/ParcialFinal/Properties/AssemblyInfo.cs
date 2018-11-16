fc1123cc171b6eba362ff9f2bb32c8c19680ab50
Santiago Betancurt <SantiagoBsk8@hotmail.com>
2018-11-16 13:32:08 -0500

    parcail hecho

--- ParcialFinal/ParcialFinal/Properties/AssemblyInfo.cs
+++ ParcialFinal/ParcialFinal/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
-[assembly: Guid("9c5f92d3-6c51-46a6-bed7-f79c13af18c9")]
+[assembly: Guid("6b00c004-dea8-4b02-add0-f57a72d8ef56")]
 
 // La información de versión de un ensamblado consta de los siguientes cuatro valores:
 //
